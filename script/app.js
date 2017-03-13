23ef45c8a7b915ebf2a608bb2e09514a39077395
Artur <a3332133@gmail.com>
2017-03-13 19:54:56 +0300

    ready homework

--- script/app.js
+++ script/app.js
@@ -20,4 +20,20 @@ function requestValidNumber(message) {
 }
 
 var data = generateArray(requestValidNumber('Введите максимальную длинну массива'));
-console.log (data);
+console.log('Первоначальный массив', data);
+
+function sort (firstArr) {
+    for (var j = 0; j < firstArr.length; j++) {
+        var max = firstArr[j];
+        for (var i = 0; i < firstArr.length; i++) {
+            if (firstArr[i] < firstArr[j]) {
+                max = firstArr[j];
+                firstArr[j] = firstArr[i];
+                firstArr[i] = max;
+            }
+        }
+    }
+    return firstArr;
+}
+var newData = sort(data);
+console.log('Отсортированный массив:',newData);
